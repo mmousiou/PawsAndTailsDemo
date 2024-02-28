d7263cdace37a1ba06504422dc2132c4ce93eb86
mmousiou <mmousiou@gmail.com>
2024-02-28 22:03:03 +0200

    Update appointmentManagementGroup1.js

--- lwc/appointmentManagementGroup1/appointmentManagementGroup1.js
+++ lwc/appointmentManagementGroup1/appointmentManagementGroup1.js
@@ -9,6 +9,13 @@ export default class AppointmentManagementGroup1 extends LightningElement {
   @track data
   showResults = false // boolean to check if we have data or not
   selectedDate // variable to store the date from input
+  columns = [
+    { label: 'Code', fieldName: 'recordUrl',  editable: false, type: 'text', initialWidth: 140},
+    { label: 'Time', fieldName: 'Time__c', editable: false, type: 'text', initialWidth: 140 },
+    { label: 'Pet Name', fieldName: 'PetName__c', editable: false, type: 'text', initialWidth: 160 },
+    { label: 'Contact Name', fieldName: 'ContactName__c', editable: false, type: 'text', initialWidth: 160 },
+    { label: 'Services', fieldName: 'Services__c', editable: false, type: 'text' },
+  ]
   
 
   // every lwc component 
